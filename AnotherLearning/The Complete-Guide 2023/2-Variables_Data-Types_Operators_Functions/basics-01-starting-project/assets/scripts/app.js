4a5468b13f7a9138887ebc41bc614cd01df62256
elektronomous <akbarfaza222@gmail.com>
2023-03-25 01:02:01 -0700

    Assignment 1 DONE

--- AnotherLearning/The Complete-Guide 2023/2-Variables_Data-Types_Operators_Functions/basics-01-starting-project/assets/scripts/app.js
+++ AnotherLearning/The Complete-Guide 2023/2-Variables_Data-Types_Operators_Functions/basics-01-starting-project/assets/scripts/app.js
@@ -3,3 +3,21 @@
 
 // 02
 let currentResult = 0;
+// 05
+// let calculationEquation = '(' + currentResult + ' + 10) * 30 + 30';
+// 06
+let calculationEquation = `(${currentResult} + 10) * 30 + 30`;
+
+currentResult = currentResult + 10;         // => 10
+// you can have any mathematical operation
+currentResult = currentResult * 30 + 30;    // => 330
+
+// pass currentResult, then change the code on the html code using it.
+outputResult(currentResult, '');    
+
+// create the equation for html to be displayed.
+// 05
+// let calculationEquation = '(0 + 10) * 30 + 30';
+
+// pass the equation
+outputResult(currentResult, calculationEquation);
